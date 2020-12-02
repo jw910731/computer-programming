9b887231fa3fd78c2be445ae7f6cc8227ae4106c
jw910731 <jw910731@gmail.com>
2020-12-02 16:50:22 +0800

    add matrix playground

---
+++ playground/matrix/matrix.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+typedef int32_t i32;
+
+typedef struct {
+	size_t w, h;
+	i32 *data;
+}Matrix;
+
+Matrix *create_mat(size_t w, size_t h);
+size_t at(Matrix *mat, size_t x, size_t y);
+Matrix *mul(Matrix *a, Matrix *b);
+void free_mat(Matrix *mat);
+void print_mat(Matrix *mat);
