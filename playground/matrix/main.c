22024c54484a786f7407979d6b2dc10b1e1757de
jw910731 <jw910731@gmail.com>
2020-12-04 23:03:10 +0800

    fix output and matrix multiply bug

--- playground/matrix/main.c
+++ playground/matrix/main.c
@@ -1,35 +1,34 @@
-#include <stdio.h>
+#include "matrix.h"
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "matrix.h"
 typedef int32_t i32;
-int main(){
-	Matrix *mat[2];
-	for(size_t ix = 0 ; ix < 2 ; ++ix){
-		size_t w, h;
-		printf("Input Matrix width and height : ");
-		scanf("%lu%lu", &w, &h);
-		mat[ix] = create_mat(w, h);
-		printf("Now Input the Matrix :\n");
-		for(size_t i = 0 ; i < w ; ++i){
-			for(size_t j = 0 ; j < h ; ++j){
-				scanf("%d", &(mat[ix]->data[at(mat[ix], i, j)]));
-			}
-		}
-	}
-	Matrix *ans = mul(mat[1], mat[0]);
-	if(ans != NULL){
-		printf("Result Matrix : \n");
-		print_mat(ans);
-	}
-	else{
-		printf("These two matrix is not multipliable.\n");
-	}
-	// free resource
-	free_mat(mat[0]);
-	free_mat(mat[1]);
-	if(ans != NULL)
-		free_mat(ans);
-	return 0;
+int main() {
+    Matrix *mat[2];
+    for (size_t ix = 0; ix < 2; ++ix) {
+        size_t w, h;
+        printf("Input Matrix width and height : ");
+        scanf("%lu%lu", &w, &h);
+        mat[ix] = create_mat(w, h);
+        printf("Now Input the Matrix :\n");
+        for (size_t i = 0; i < w; ++i) {
+            for (size_t j = 0; j < h; ++j) {
+                scanf("%d", &(mat[ix]->data[at(mat[ix], i, j)]));
+            }
+        }
+    }
+    Matrix *ans = mul(mat[1], mat[0]);
+    if (ans != NULL) {
+        printf("Result Matrix : \n");
+        print_mat(ans);
+    } else {
+        printf("These two matrix is not multipliable.\n");
+    }
+    // free resource
+    free_mat(&mat[0]);
+    free_mat(&mat[1]);
+    if (ans != NULL)
+        free_mat(&ans);
+    return 0;
 }
--- playground/matrix/matrix.c
+++ playground/matrix/matrix.c
@@ -1,40 +1,41 @@
 #include "matrix.h"
 #include <stdio.h>
 // constructor
-Matrix *create_mat(size_t w, size_t h){
-	Matrix *ret = calloc(1, sizeof(Matrix));
-	ret->data = calloc(w*h, sizeof(i32));
-	ret->w = w;
-	ret->h = h;
-	return ret;
+Matrix *create_mat(size_t w, size_t h) {
+    Matrix *ret = calloc(1, sizeof(Matrix));
+    ret->data = calloc(w * h, sizeof(i32));
+    ret->w = w;
+    ret->h = h;
+    return ret;
 }
-size_t at(Matrix *mat, size_t x, size_t y){
-	return x+(y*mat->w);
-}
-Matrix *mul(Matrix *a, Matrix *b){
-	if(a->w != b->h) return NULL;
-	Matrix *ret = create_mat(b->w, a->h);
-	for(size_t i = 0 ; i < a->h ; ++i){ // y idx
-		for(size_t j = 0 ; j < b->w ; ++j){ // x idx
-			// mul a->row and b->col
-			for(size_t k = 0 ; k < b->h ; ++k){
-				ret->data[at(ret, j, i)] += b->data[at(b, j, k)] * a->data[at(a, k, i)];
-			}
-		}
-	}
-	return ret;
+size_t at(Matrix *mat, size_t x, size_t y) { return x + (y * mat->w); }
+Matrix *mul(Matrix *a, Matrix *b) {
+    if (a->w != b->h)
+        return NULL;
+    Matrix *ret = create_mat(b->w, a->h);
+    for (size_t i = 0; i < a->h; ++i) {     // y idx
+        for (size_t j = 0; j < b->w; ++j) { // x idx
+            // mul a->row and b->col
+            for (size_t k = 0; k < b->h; ++k) {
+                ret->data[at(ret, i, j)] +=
+                    b->data[at(b, j, k)] * a->data[at(a, k, i)];
+            }
+        }
+    }
+    return ret;
 }
 // destructor
-void free_mat(Matrix *mat){
-	free(mat->data);
-	free(mat);
+void free_mat(Matrix **mat) {
+    free((*mat)->data);
+    free(*mat);
+    *mat = NULL;
 }
-// print matrix 
-void print_mat(Matrix *mat){
-	for(size_t i = 0 ; i < mat->h ; ++i){
-		for(size_t j = 0 ; j < mat->w ; ++j){
-			printf("%3d ", mat->data[at(mat, i, j)]);
-		}
-		putchar('\n');
-	}
+// print matrix
+void print_mat(Matrix *mat) {
+    for (size_t i = 0; i < mat->h; ++i) {
+        for (size_t j = 0; j < mat->w; ++j) {
+            printf("%3d ", mat->data[at(mat, j, i)]);
+        }
+        putchar('\n');
+    }
 }
