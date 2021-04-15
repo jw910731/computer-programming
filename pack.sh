f6c463bbca74dfa989ef0e86465287367341e90a
jw910731 <jw910731@gmail.com>
2021-04-16 00:02:49 +0800

    Add global gitignore to zip ignore

--- pack.sh
+++ pack.sh
@@ -11,5 +11,5 @@ origin_dir=hw0$1
 dest_name=${id}_hw0$1
 
 ln -s $origin_dir $dest_name
-zip -r ${dest_name}.zip ${dest_name} -x "*.DS_Store" "*.gitignore" -x $([ -r ${dest_name}/.gitignore ] && cat ${dest_name}/.gitignore | perl -pe 's/^(?!\*)/\*/g' | sed -E "s/(.+)\/\*\*/\1\*\n\1\*/g")
+zip -r ${dest_name}.zip ${dest_name} -x "*.DS_Store" "*.gitignore" -x $([ -r ${dest_name}/.gitignore ] && cat ${dest_name}/.gitignore | perl -pe 's/^(?!\*)/\*/g' | sed -E "s/(.+)\/\*\*/\1\*\n\1\*/g") -x $([ -r .gitignore ] && cat .gitignore | perl -pe 's/^(?!\*)/\*/g' | sed -E "s/(.+)\/\*\*/\1\*\n\1\*/g")
 rm $dest_name
