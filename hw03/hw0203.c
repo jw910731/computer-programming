71eb81a3e37a4a3175c026278a554d0483ffa785
jw910731 <jw910731@gmail.com>
2021-04-21 14:08:49 +0800

    Complete file read of hw0303

--- hw03/hw0203.c
+++ hw03/hw0203.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "basic.h"
+
+#define PI (acos(-1))
 
 struct bmpHeader{
     char bm[2];
@@ -22,27 +25,10 @@ struct bmpHeader{
     uint32_t important;
 }__attribute__ ((__packed__));
 
-struct pixelMask{
-    uint32_t r, g, b, a;
-}__attribute__ ((__packed__));
-
 struct pix24_t{
     uint8_t b;
     uint8_t g;
     uint8_t r;
-}__attribute__ ((__packed__));
-
-struct pix32_t{
-    uint8_t b;
-    uint8_t g;
-    uint8_t r;
-    uint8_t a : 5;
-    uint8_t unused : 3;
-}__attribute__ ((__packed__));
-
-union pix_union{
-    struct pix24_t b24;
-    struct pix32_t b32;
 }__attribute__ ((__packed__));
 
 int main() {
@@ -65,46 +51,30 @@ int main() {
         return 1;
     }
 
-    struct bmpHeader inHeader, outHeader;
-    fread( &inHeader, 54, 1, inFile );
-    memcpy(&outHeader, &inHeader, sizeof(struct bmpHeader));
-    if(inHeader.bpp != 24){
-        puts("Input picture is not 24 bit color depth.");
-        return 1;
-    }
-
-    i32 alpha = 0, ret = -2;
+    i32 angle = 0, ret = -2;
 
     do{
         if(ret != -2){
             puts("Input Error!");
         }
-        printf("Alpha (0-31): ");
-        ret = scanf("%d", &alpha);
-        if(alpha < 0 || alpha > 31){
+        printf("Angle (0-90): ");
+        ret = scanf("%d", &angle);
+        if(angle < 0 || angle > 90){
             ret = 0;
         }
         while(getchar() != '\n');
     }while(ret != 1);
 
+    double rad_angle = (angle/180.0) * PI;
+
+    struct bmpHeader inHeader, outHeader;
+    fread( &inHeader, 54, 1, inFile );
+    memcpy(&outHeader, &inHeader, sizeof(struct bmpHeader));
+
     size_t header_len = sizeof(struct bmpHeader), bm_len = 0;
     fseek(outFile, sizeof(struct bmpHeader), SEEK_CUR);
 
-    struct pixelMask mask = {.a = 0x1f000000, .r = 0x00ff0000, .g = 0x0000ff00, .b = 0x000000ff};
-    header_len += fwrite(&mask, 1, sizeof(mask), outFile);
-
-    outHeader.offset = header_len;
-    outHeader.bpp = 32;
-    outHeader.compression = 6;
-
-    union pix_union pixel;
-
-    while(1){
-        fread(&pixel.b24, 1, sizeof(struct pix24_t), inFile);
-        if(feof(inFile)) break;
-        pixel.b32.a = alpha;
-        bm_len += fwrite(&pixel.b32, 1, sizeof(struct pix32_t), outFile);
-    }
+    // TODO: Process picture
 
     outHeader.size = header_len + bm_len;
     outHeader.bitmap_size = bm_len;
@@ -116,3 +86,4 @@ int main() {
     fclose(outFile);
     return 0;
 }
+
