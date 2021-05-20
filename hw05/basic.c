8a58fa000b88bc425e97fbc5ff3f5101d1770378
jw910731 <jw910731@gmail.com>
2021-05-20 16:17:14 +0800

    Create hw05 with template

---
+++ hw05/basic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void err(const char *s){
+	fprintf(stderr, "%s\nThus program is going to terminate.\n", s);
+	exit(1);
+}
+
+size_t fgets_n(char *s, int size, FILE *stream){
+    char *ptr = fgets(s, size, stream);
+    if(ptr != NULL){
+        size_t ret = strlen(ptr);
+        if(ptr[ret-1] == '\n'){
+            ptr[--ret] = 0;
+        }
+        return ret;
+    }
+    return 0;
+}
