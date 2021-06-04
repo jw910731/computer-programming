0a4f95679cad7dbdb5895b5992f51ad784e293a4
jw910731 <jw910731@gmail.com>
2021-06-05 00:45:20 +0800

    Update pack.sh to remove target prefix

--- pack.sh
+++ pack.sh
@@ -7,8 +7,8 @@ if [ -z ${1+x} ]; then
 	exit
 fi
 
-origin_dir=hw0$1
-dest_name=${id}_hw0$1
+origin_dir=$1
+dest_name=${id}_$1
 
 ln -s $origin_dir $dest_name
 zip -r ${dest_name}.zip ${dest_name} -x "*.DS_Store" "*.gitignore" -x $([ -r ${dest_name}/.gitignore ] && cat ${dest_name}/.gitignore | sed -E 's/#.*$//g' | perl -pe 's/^(?!\*)/\*/g' | sed -E "s/(.+)\/\*\*/\1\*\n\1\*/g") -x $([ -r .gitignore ] && cat .gitignore | sed -E 's/#.*$//g' | perl -pe 's/^(?!\*)/\*/g' | sed -E "s/(.+)\/\*\*/\1\*\n\1\*/g")
